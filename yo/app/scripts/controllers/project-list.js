9d44f8fb16392728898f4ff585f1c00526d3b2f3
Jan Polowinski <jan.polowinski@slub-dresden.de>
2016-06-07 09:45:20 +0200

    [WIP] basic integration of temp-dswarm-utils (mapping table)

--- yo/app/scripts/controllers/project-list.js
+++ yo/app/scripts/controllers/project-list.js
@@ -15,8 +15,27 @@
  */
 'use strict';
 
-var baseUrlBackend = 'http://194.95.142.135/dmp/';
-
+function parseFilterExpressions(projects) {
+	
+	for (let project of projects) { // 'for i of var' iterates the values, for .. in .. iterates the keys instead (ECMA 2015) https://developer.mozilla.org/de/docs/Web/JavaScript/Reference/Statements/for...of#Browser_compatibility 
+//	   console.log(project); 
+	   for (let mapping of project.mappings) {
+//		   console.log(mapping);
+		   for (let iap of mapping.input_attribute_paths) {
+//			   console.log(iap);
+			   if (iap.filter!=null && iap.filter.expression!=null) {
+//				   console.log(iap.filter.expression);
+				   var parsedFilterExpression = jQuery.parseJSON(iap.filter.expression);
+//				   console.log(parsedFilterExpression);
+				   iap.filter.expression=parsedFilterExpression;
+//				   console.log(iap.filter.expression);
+			   }
+		   }
+	   }
+		   
+		}
+	return projects;
+}
 
 
 angular.module('dmpApp')
@@ -42,9 +61,7 @@ angular.module('dmpApp')
       getFilterExpression: function(imapi) {
     	  var expressionString;
     	  var expression = null;
-    	  if(imapi.filter==null) 
-    		  return null;
-    	  else if (imapi.filter.expression!=null) {
+    	  if (imapi.filter!=null && imapi.filter.expression!=null) {
     		  expressionString = imapi.filter.expression;
 //    		  expression = {"type" : "test", "expression" : "expression"};
     		  expression = jQuery.parseJSON(expressionString);
@@ -85,8 +102,7 @@ angular.module('dmpApp')
   .controller('ProjectsCtrl', function($scope, $http, MappingTable, ApiEndpoint){
 	// TODO use ProjectsResource or even reuse project list from overview page
     $http.get(ApiEndpoint + 'projects?format=medium').then(function(response) {
-
-      $scope.projects = response.data;
+      $scope.projects = parseFilterExpressions(response.data);
       $scope.mappingTable = MappingTable;
     });
   })
